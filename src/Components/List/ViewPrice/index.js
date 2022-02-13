224121379a6d5ecbfd4e8d6cd7e4089a7eaa1944
trucdongtxtv <trucdong.txtv@gmail.com>
2022-02-13 08:48:31 +0700

    Price Editor

--- src/Components/List/ViewPrice/index.js
+++ src/Components/List/ViewPrice/index.js
@@ -6,8 +6,7 @@ import styled from 'styled-components';
 import {Header} from '@components/Header';
 
 import {useDispatch, useSelector} from 'react-redux';
-import {addParkingLotAction, registerAction} from '@store/actionTypes';
-import {Picker} from '@react-native-picker/picker';
+import {PriceEditor} from '../shared/PriceEditor';
 
 const Container = styled.SafeAreaView`
   flex: 1;
@@ -16,22 +15,29 @@ const Container = styled.SafeAreaView`
 `;
 const FieldArea = styled.View`
   align-self: center;
+  width: 100%;
+  padding-top: 20px;
+  background-color: #121212;
+  flex-direction: row;
+`;
+const SelectorArea = styled.View`
+  align-self: center;
   flex: 1;
   width: 100%;
-  padding-top: 50px;
-  margin-bottom: -50px;
+  padding-horizontal: 20px;
+  padding-top: 7px;
+  padding-bottom: 25px;
   background-color: #121212;
 `;
 const Label = styled.Text`
   font-size: 16px;
-  margin-horizontal: 40px;
+  margin-left: 40px;
+  align-self: center;
   color: white;
-`;
-const BlackLabel = styled(Label)`
-  color: black;
 `;
 const Input = styled.TextInput`
   height: 45px;
+  flex: 1;
   font-size: 18px;
   border-radius: 5px;
   background-color: #424242;
@@ -40,107 +46,19 @@ const Input = styled.TextInput`
   margin-vertical: 8px;
   margin-horizontal: 40px;
 `;
-const Button = styled.TouchableOpacity`
-  background-color: #ffb500;
-  flex: 1;
-  align-items: center;
-  justify-content: center;
-  border-radius: 5px;
-  margin-horizontal: 40px;
-`;
-const ButtonArea = styled.View`
-  height: 50px;
-  flex-direction: row;
-  margin-top: 25px;
-`;
-const Space = styled.View`
-  height: ${props => props.height || 500}px;
-`;
-const PriceRow = styled.View`
-  flex-direction: row;
-  height: 30px;
-  margin-top: 10px;
-  justify-content: center;
-  align-items: center;
-  align-content: center;
-  overflow: hidden;
-  margin-left: 38px;
-  margin-right: 40px;
-`;
-const PricePicker = styled(Picker)`
-  flex: 1;
-  height: 30px;
-  top: -93px;
-  margin-horizontal: -7px;
-`;
-const PricePickerItem = styled(Picker.Item)`
-  font-size: 14px;
-`;
-const PriceField = styled.TextInput`
-  font-size: 17px;
-  flex: 2;
-  text-align: right;
-  color: white;
-`;
-const PriceLabel = styled.Text`
-  font-size: 16px;
-  color: white;
-`;
-
-const hourValue = [...Array(24).keys()];
-const minuteValue = [...Array(60).keys()];
-const valueString = i => (
-  <PricePickerItem
-    label={i.toLocaleString('en-US', {
-      minimumIntegerDigits: 2,
-    })}
-    value={i}
-    color="white"
-  />
-);
 
 const Screen = ({}) => {
   const [name, setName] = useState('');
-  const [address, setAddress] = useState('');
-  const [spaceCount, setSpaceCount] = useState('');
 
   const [price, setPrice] = useState([]);
-  const [mark, setMark] = useState([0, 0]);
+  const [mark, setMark] = useState([]);
 
   const dispatch = useDispatch();
   const navigation = useNavigation();
 
-  const register = () => {
-    dispatch(addParkingLotAction(address, name, parseInt(spaceCount)));
-    navigation.goBack();
-  };
-
-  const renderTimePicker = mark => {
-    const hour = Math.floor(mark);
-    const minute = Math.round((mark - hour) * 60);
-
-    const style = {
-      color: 'white',
-    };
-
-    return (
-      <>
-        <PricePicker selectedValue={hour}>
-          {hourValue.map(valueString)}
-        </PricePicker>
-        <PriceLabel>:</PriceLabel>
-        <PricePicker selectedValue={minute}>
-          {minuteValue.map(valueString)}
-        </PricePicker>
-      </>
-    );
-  };
-
-  const renderFrom = mark => {
-    return renderTimePicker(mark);
-  };
-  const renderTo = mark => {
-    return renderTimePicker(mark);
+  const handleChange = newSchedule => {
+    console.log(newSchedule);
+    setMark(newSchedule);
   };
 
   return (
@@ -151,36 +69,12 @@ const Screen = ({}) => {
         goBack={() => navigation.goBack()}
       />
       <FieldArea>
-        <Label>Location name</Label>
-        <Input onChangeText={text => setName(text)} />
-        <Label>Address</Label>
-        <Input onChangeText={text => setAddress(text)} />
-        <Label>Space count</Label>
-        <Input
-          keyboardType="numeric"
-          onChangeText={text => setSpaceCount(text)}
-        />
-        <Space height={20} />
         <Label>Price</Label>
-        {[...Array(mark.length - 1).keys()].map(i => (
-          <PriceRow>
-            {renderFrom(mark[i])}
-            <PriceLabel> to </PriceLabel>
-            {renderTo(mark[i + 1])}
-            <PriceField
-              placeholder="Enter price"
-              placeholderTextColor="#777777"
-            />
-          </PriceRow>
-        ))}
-
-        <ButtonArea>
-          <Button onPress={register}>
-            <BlackLabel>Add parking lot</BlackLabel>
-          </Button>
-        </ButtonArea>
-        <Space />
+        <Input onChangeText={text => setName(text)} />
       </FieldArea>
+      <SelectorArea>
+        <PriceEditor />
+      </SelectorArea>
     </Container>
   );
 };
---
+++ src/Components/List/shared/PriceEditor/index.js
@@ -0,0 +1,134 @@
+import React, {useEffect, useRef, useState} from 'react';
+import styled from 'styled-components';
+import {Slider} from '@miblanchard/react-native-slider';
+import {useDispatch} from 'react-redux';
+
+const Container = styled.View`
+  flex: 1;
+  width: 100%;
+`;
+const ScrollArea = styled.ScrollView`
+  padding-top: 10px;
+`;
+const Space = styled.View`
+  height: 30px;
+`;
+const HorizArea = styled.View`
+  flex-direction: row;
+  height: 30px;
+  margin-horizontal: 30px;
+`;
+const VertArea = styled.View`
+  width: 30px;
+  margin-top: -7px;
+  margin-bottom: -30px;
+`;
+const Label = styled.Text`
+  font-size: 13px;
+  color: #808080;
+  flex: 1;
+  text-align: center;
+  align-self: center;
+`;
+const VertLabel = styled(Label)`
+  text-align: right;
+  align-self: flex-end;
+  margin-right: 5px;
+  overflow: visible;
+`;
+const RowArea = styled.View`
+  flex: 1;
+  flex-direction: row;
+  margin-bottom: 0.5px;
+`;
+const Area = styled.View`
+  flex: 1;
+`;
+const Cell = styled.View`
+  flex: 1;
+  height: 36px;
+  background-color: #424242;
+  margin: 0.5px;
+`;
+const ScrollbarArea = styled.View`
+  width: 500px;
+  align-items: stretch;
+  align-self: center;
+  top: 240px;
+  margin-left: 6px;
+`;
+
+const day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+const hour = [...Array(24).keys()];
+
+export function PriceEditor({}) {
+  const scrollRef = useRef();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    scrollRef?.current.scrollTo({
+      y: (sizeY * 0.61) / 2.2,
+      animated: false,
+    });
+  });
+
+  const [sizeX, setSizeX] = useState(0);
+  const [sizeY, setSizeY] = useState(0);
+
+  const scrollTo = val => {
+    if (sizeY > 0) {
+      scrollRef?.current.scrollTo({
+        y: (sizeY * (1 - val)) / 2.2,
+        animated: false,
+      });
+    }
+  };
+
+  console.log(sizeX, sizeY);
+
+  return (
+    <Container>
+      <HorizArea>
+        {day.map(d => (
+          <Label key={`horiz_${d}`}>{d}</Label>
+        ))}
+      </HorizArea>
+      <RowArea>
+        <ScrollArea
+          ref={scrollRef}
+          scrollEventThrottle={1}
+          showsVerticalScrollIndicator={false}
+          onContentSizeChange={(w, h) => {
+            setSizeX(w);
+            setSizeY(h);
+          }}
+          scrollEnabled={false}>
+          <RowArea>
+            <VertArea>
+              {hour.map(h => (
+                <VertLabel key={`vert_${h}`}>{h % 24}h</VertLabel>
+              ))}
+              <VertLabel>0h</VertLabel>
+            </VertArea>
+            <Area>
+              {hour.map(h => (
+                <RowArea key={`row_${h}`}>
+                  {day.map(d => (
+                    <Cell key={`cell_${d}_${h}`} />
+                  ))}
+                </RowArea>
+              ))}
+            </Area>
+          </RowArea>
+          <Space />
+        </ScrollArea>
+
+        <VertArea>
+          <ScrollbarArea>
+            <Slider value={0.39} vertical onValueChange={scrollTo} />
+          </ScrollbarArea>
+        </VertArea>
+      </RowArea>
+    </Container>
+  );
+}
