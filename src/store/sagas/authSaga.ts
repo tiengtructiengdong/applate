29f02e5a86bb60f28c14a50f8738232403397dab
trucdongtxtv <trucdong.txtv@gmail.com>
2022-03-16 09:21:21 +0700

    Fix

--- src/store/sagas/authSaga.ts
+++ src/store/sagas/authSaga.ts
@@ -18,7 +18,8 @@ const registerSaga = function* (action: AnyAction) {
     const response = yield* call(register, registerData);
     if (response.status === 400) {
       console.log(response);
-      throw new Error('ID or Phone number\nis registered!');
+      const errorMessage = response?.data?.message;
+      throw new Error(errorMessage || 'ID or Phone number\nis registered!');
     }
     popUp('Registration success!', 'You can continue with the app.');
     onSuccess();
